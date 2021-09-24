3bc35a9983b7c2068d25bd257b405622da062dad
Oliver Pearce <oliver.pearce@husqvarnagroup.com>
2021-09-24 22:20:18 +0200

    feat: further setup work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Question } from './Models/Question';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('question')
+  getQuestionSet(): Array<Question> {
+    const question = new Question();
+    question.message = 'this is a test question';
+    const questionSet = new Array<Question>();
+    questionSet.push(question);
+    return questionSet;
   }
 }
